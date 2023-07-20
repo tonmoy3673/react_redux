8a354fb6acf6207a8f6861fc51a4bf3660340cbe
tonmoy <tonmoy3673@gmail.com>
2023-07-21 00:40:12 +0600

    React Counter App

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -2,15 +2,24 @@ import React, { useState } from 'react';
 
 const Counter = () => {
     const [count,setCount]=useState(0);
+
     const handleAdd=()=>{
-        setCount((count)=>count+1)
+        setCount((count)=>count+1);
     }
+    const handleRemove=()=>{
+        setCount((count)=>count-1);
+    }
+    const handleReset=()=>{
+        setCount((count)=>count=0);
+    }
+
     return (
         <div>
-            <h2>Counter App</h2>
+            <h2>React Counter App</h2>
             <h3>Count : {count}</h3>
             <button onClick={handleAdd}>Add</button>
-
+            <button onClick={handleRemove}>Remove</button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
 };
