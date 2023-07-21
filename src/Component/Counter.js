bf10f565cbde31b5950358ac9c2f8279ee225a15
tonmoy <tonmoy3673@gmail.com>
2023-07-22 01:55:54 +0600

    set count react app

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+    const [count,setCount]=useState(0);
+    const handleAdd=()=>{
+      setCount((count+1))
+    }
+    const handleReset=()=>{
+      setCount(0)
+    }
+    const handleRemove=()=>{
+        setCount(count-1)
+      }
     return (
         <div>
             <h2>Counter App</h2>
-            <h3>Count : 0</h3>
+            <h3>Count : {count}</h3>
+            <button onClick={handleAdd}>Add</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleRemove}>Remove</button>
         </div>
     );
 };
