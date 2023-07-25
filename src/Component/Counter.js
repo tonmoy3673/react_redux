78eb64a436b2b3333da144e44f0ce8861aba34df
tonmoy <tonmoy3673@gmail.com>
2023-07-26 01:40:08 +0600

    implement add

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCount } from './Action/Action';
+
+
 
 const Counter = () => {
+  const count=useSelector(state=>state.count);
+  const dispatch=useDispatch();
+  const handleAdd=()=>{
+    dispatch(addCount())
+  }
     return (
         <div>
             <h1>React Counter App by Redux</h1>
-            <h3>Count : 0</h3>
+            <h3>Count : {count}</h3>
+            <div>
+                <button onClick={handleAdd}>Add</button>
+            </div>
         </div>
     );
 };
