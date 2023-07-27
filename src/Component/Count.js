b8ac751dab133829b6ee8c1aa20cc0ee1b6ad78d
tonmoy <tonmoy3673@gmail.com>
2023-07-28 02:01:05 +0600

    setup countState

--- src/Component/Count.js
+++ src/Component/Count.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+
 
 const Count = () => {
+    const count=useSelector(state=>state.count.count);
+    console.log(count)
     return (
         <div>
             <h1>Count App with ToolKit</h1>
+            <div>
+                <h3>Count : 0</h3>
+            </div>
         </div>
     );
 };
