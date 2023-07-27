55789a0e7f1aae4f0f478a5f6f81d574e645a6a4
tonmoy <tonmoy3673@gmail.com>
2023-07-28 01:54:45 +0600

    setup store

--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -1,5 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
 import Reducer from "../Reducer";
 import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+import CounterSlice from "../CounterSlice";
 
-export const  store=createStore(Reducer,applyMiddleware(thunk))
+export const  store=createStore(Reducer,applyMiddleware(thunk));
+export const myStore=configureStore({
+    reducer:{
+        count:CounterSlice
+    }
+})
