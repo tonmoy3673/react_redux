f962db672a9708a516415bea2fdd0e942455e463
tonmoy <tonmoy3673@gmail.com>
2023-07-27 12:49:15 +0600

    modify all count

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addCount } from './Action/Action';
+import { addCount, removeCount, resetCount } from './Action/Action';
 
 const Counter = () => {
     const count=useSelector(state=>state.count);
     const dispatch=useDispatch();
     const handleAdd=()=>{
         dispatch(addCount())
-    }
+    };
+    const handleReset=()=>{
+        dispatch(resetCount())
+    };
+
+    const handleRemove=()=>{
+        dispatch(removeCount())
+    };
+
     return (
         <div>
             <h2> React Redux Counter App</h2>
             <div>
                 <h4>Count : {count}</h4>
                 <button onClick={handleAdd}>Add</button>
+                <button onClick={handleReset}>Reset</button>
+                <button onClick={handleRemove}>Remove</button>
 
             </div>
         </div>
