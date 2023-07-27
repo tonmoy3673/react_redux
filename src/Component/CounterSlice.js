985b0257ed9d285a5a534358e502e4757cf510a9
tonmoy <tonmoy3673@gmail.com>
2023-07-28 01:46:32 +0600

    setup toolkit

---
+++ src/Component/CounterSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CounterSlice=createSlice({
+    name:'CountApp',
+    initialState:{
+        count:0
+    },
+    reducers:{
+        ADD: state=>{
+            state.count=state.count+1
+        },
+        RESET : state=>{
+            state.count=0
+        },
+
+        REMOVE : state=>{
+            state.count=state.count-1
+        }
+    }
+})
