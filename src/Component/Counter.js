495935ac80946646d92726bc5da51f5d129248aa
tonmoy <tonmoy3673@gmail.com>
2023-07-30 23:42:35 +0600

    modify counter

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,7 +1,4 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addCount, removeCount, resetCount } from './Action/Action';
-import { todoAction } from './Action/TodoAction';
 
 const Counter = () => {
    
@@ -11,32 +8,7 @@ const Counter = () => {
     return (
         <div>
             <h2> React Redux Counter App</h2>
-            <div>
-                <h4>Count : {count}</h4>
-                <button onClick={handleAdd}>Add</button>
-                <button onClick={handleReset}>Reset</button>
-                <button onClick={handleRemove}>Remove</button>
-
-            </div>
-
-           <div>
-            {
-                isLoading && <h4> Loading ... : 'Not Found'</h4>
-            }
-            {
-                error && <h4> {error.message} : 'Null'</h4>
-            }
-
-            <section>
-                {
-                    todo && todo.map(list=>{return <article key={list.id}>
-                        <h5>{list.title}</h5>
-                        <h5>{list.userId}</h5>
-                    </article>})
-                        
-                }
-            </section>
-           </div>
+            
 
         </div>
     );
