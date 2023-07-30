f542ddaa0b15a35cf68c291f8cab8123cfa4213e
tonmoy <tonmoy3673@gmail.com>
2023-07-30 23:43:09 +0600

    modify store

--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -1,6 +1,1 @@
-import { applyMiddleware, createStore } from "redux";
-import Reducer from "../Reducer";
-import thunk from "redux-thunk";
 
-
-export const  store=createStore(Reducer,applyMiddleware(thunk));
