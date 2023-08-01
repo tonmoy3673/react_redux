0e0fefa62ea1e60cc8e5aad9000cc69bf305d344
tonmoy <tonmoy3673@gmail.com>
2023-08-02 01:58:07 +0600

    set todos

--- src/Component/Action/TodoAction.js
+++ src/Component/Action/TodoAction.js
@@ -1,10 +1,24 @@
 
 
-export const TodoAction =async() => {
+export const resTodo=()=>{
+    return{
+        type:'RES'
+    }
+}
+export const showTodo=(todo)=>{
+    return{
+        type:'SHOW',
+        payload:todo
+    }
+}
+export const failTodo=(error)=>{
+    return{
+        type:'FAIL',
+        payload:error
+    }
+}
 
 
-
-};
 
 
 
--- src/Component/Reducer.js
+++ src/Component/Reducer.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+import { resTodo } from "./Action/TodoAction";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 
 const initialState={
     count:0,
@@ -23,10 +27,55 @@ const Reducer = (state=initialState,action) => {
                 ...state,
                 count:state.count-1,
             }
+            case 'RES':
+            return{
+                ...state,
+                isLoading:true,
+            }
+            case 'SHOW':
+            return{
+                ...state,
+                isLoading:false,
+                todo:action.payload
+            }
+            case 'FAIL':
+            return{
+                ...state,
+                isLoading:false,
+                error:action.payload
+            }
     
         default:
             return state;
     }
 };
 
+const fetchData=()=>{
+    return(dispatch)=>{
+        dispatch(resTodo())
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+        .then(res=>{
+            const myTodo=res.data
+          
+            const title=myTodo.map(list=>list.title)
+            console.log(title)
+            dispatch(resTodo(title))
+        })
+        .catch((error)=>{
+            const errorSms=error.message
+            dispatch(errorSms)
+        })
+            
+    }
+
+}
+
+
+
+
+ const store=createStore(Reducer,applyMiddleware(thunk));
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+store.dispatch(fetchData())
 export default Reducer;
