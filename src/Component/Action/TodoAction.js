a4c7093e4569f12dfd8209a24f1dc2314fc219f4
tonmoy <tonmoy3673@gmail.com>
2023-08-03 01:40:36 +0600

    set action

--- src/Component/Action/TodoAction.js
+++ src/Component/Action/TodoAction.js
@@ -1,3 +1,17 @@
+import axios from 'axios'
+
+
+export const TodoAction = ()=>async(dispatch) => {
+    
+    dispatch({type:'RES'})
+    try {
+       const res=await axios.get('https://jsonplaceholder.typicode.com/todos')
+       dispatch({type:'SHOW',payload:res.data})
+    } catch (error) {
+        dispatch({type:'FAIL',payload:error.message})
+    }
+
+};
 
 
 
