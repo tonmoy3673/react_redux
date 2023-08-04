5925e1a994731b095f9e84e788c9cbdcafd55b98
tonmoy <tonmoy3673@gmail.com>
2023-08-04 20:42:17 +0600

    set toolkit reducer

--- src/Component/CounterSlice.js
+++ src/Component/CounterSlice.js
@@ -1,8 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const CounterSlice = createSlice({
-    name:'count',
+export const CounterSlice = createSlice({
+    name:'counter',
+    initialState:{
+        count:0
+    },
+
+    reducers:{
+        add:state=>{
+          state.count=state.count+1
+        },
+
+        reset:state=>{
+            state.count=0
+          },
+
+          remove:state=>{
+            state.count=state.count-1
+          },
+    },
+
+  
 
 });
+
+export const {add,reset,remove}=CounterSlice.actions;
+export default CounterSlice.reducer;
 
