0f01be4a7ae55f26d47b43772ffcf3768ab3b61f
tonmoy <tonmoy3673@gmail.com>
2023-08-05 01:34:05 +0600

    set all button

--- src/Component/Count.js
+++ src/Component/Count.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD, RESET } from './CounterSlice';
 
 const Count = () => {
 
     const {count}=useSelector((state)=>(state.counter));
+    const dispatch=useDispatch()
     return (
         <div>
             <h2>React Redux Toolkit Counter App</h2>
             <h3>Count : {count}</h3>
+
+            <button onClick={()=>dispatch(ADD())}>ADD</button>
+            <button onClick={()=>dispatch(RESET())}>RESET</button>
+            <button onClick={()=>dispatch()}>REMOVE</button>
         </div>
     );
 };
