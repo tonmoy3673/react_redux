379dd391007d77198178f71a686d99fa223ec159
tonmoy <tonmoy3673@gmail.com>
2023-08-06 00:17:43 +0600

    build postView

---
+++ src/Component/PostSlice/postView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const postView = () => {
+    return (
+        <div>
+            <h2>Show All Posts</h2>
+        </div>
+    );
+};
+
+export default postView;
--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "../CounterSlice";
+import { postReducer } from "../PostSlice/PostSlice";
 
 
 export const store=configureStore({
     reducer:{
-        counter:CounterReducer
+        counter:CounterReducer,
+        posts:postReducer
     }
 });
