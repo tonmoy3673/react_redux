325ec15f32e83487a23ed0dacfe249b4005633ca
tonmoy <tonmoy3673@gmail.com>
2023-08-06 00:05:46 +0600

    set state

--- src/Component/PostSlice/PostSlice.js
+++ src/Component/PostSlice/PostSlice.js
@@ -2,5 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const postSlice=createSlice({
     name:'posts',
-
+    initialState:{
+        isLoading:false,
+        posts:[],
+        error:null
+    }
 });
