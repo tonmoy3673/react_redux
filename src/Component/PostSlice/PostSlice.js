e634c885486e65127549373dcb90341771c064ea
tonmoy <tonmoy3673@gmail.com>
2023-08-09 00:22:46 +0600

    set reducer

--- src/Component/PostSlice/PostSlice.js
+++ src/Component/PostSlice/PostSlice.js
@@ -5,7 +5,7 @@ export const fetchPost=createAsyncThunk('posts/fetchPost',async()=>{
     const res=await axios.get('https://jsonplaceholder.typicode.com/posts')
     return res.data
 })
-const PostSlice=createSlice({
+export const PostSlice=createSlice({
     name:'posts',
     initialState:{
         isLoading:false,
@@ -14,6 +14,21 @@ const PostSlice=createSlice({
     },
 
     extraReducers:(builder)=>{
-       
+       builder.addCase(fetchPost.pending,state=>{
+            state.isLoading=true;
+            state.post=[];
+            state.error=null;
+       });
+       builder.addCase(fetchPost.fulfilled,(state,action)=>{
+        state.isLoading=false;
+        state.post=action.payload;
+        state.error=null;
+   });
+   builder.addCase(fetchPost.pending,(state,action)=>{
+    state.isLoading=true;
+    state.error=action.error.message;
+});
     }
-})
+});
+
+export default PostSlice.reducer;
