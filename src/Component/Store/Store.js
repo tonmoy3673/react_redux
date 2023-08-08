d2ef9515dc75140be735a2c95109e55ea7339d59
tonmoy <tonmoy3673@gmail.com>
2023-08-09 00:27:00 +0600

    set store

--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "../CounterSlice";
+import PostReducer from "../PostSlice/PostSlice";
 
 export const store=configureStore({
     reducer:{
-        count:CounterReducer
+        count:CounterReducer,
+        post:PostReducer,
     }
 });
