a117a067b57b8c412d1ac4aa1b4a78b2cb536992
tonmoy <tonmoy3673@gmail.com>
2023-08-09 00:30:58 +0600

    set postView File

---
+++ src/Component/PostSlice/postView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PostView = () => {
+    return (
+        <div>
+            <h2>React Redux Toolkit Posts View</h2>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default PostView;
