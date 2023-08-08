7b3d2d9755917e8230508a06cf1ebe80bd6e557e
tonmoy <tonmoy3673@gmail.com>
2023-08-08 23:34:43 +0600

    set counter fike

---
+++ src/Component/Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <h2>React Redux Counter App</h2>
+            <div>
+                <h3>Count : 0</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Counter;
