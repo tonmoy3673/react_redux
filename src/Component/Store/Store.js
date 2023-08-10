8bdcfcbede68b65445120635fb97889c771f8621
tonmoy <tonmoy3673@gmail.com>
2023-08-11 00:06:06 +0600

    set todo store

--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "../CounterSlice";
 import PostReducer from "../PostSlice/PostSlice";
+import TodoReducer from "../TodoSlice/TodoSlice";
 
 export const store=configureStore({
     reducer:{
         count:CounterReducer,
         post:PostReducer,
+        todo:TodoReducer,
     }
 });
