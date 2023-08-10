183da192f1c0ae0e215a8ae93d96439ed10aa1fd
tonmoy <tonmoy3673@gmail.com>
2023-08-10 23:43:56 +0600

    setup state

--- src/Component/TodoSlice/TodoSlice.js
+++ src/Component/TodoSlice/TodoSlice.js
@@ -1,5 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const TodoSlice=createSlice({
+    name:'todo',
+    initialState:{
+        isLoading:false,
+        todo:[],
+        error:null
+    },
     
 })
