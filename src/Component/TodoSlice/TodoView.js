1e8ba6aa664b8e187691372a60f6a6485f5e2e0a
tonmoy <tonmoy3673@gmail.com>
2023-08-11 00:09:52 +0600

    set todo view file

---
+++ src/Component/TodoSlice/TodoView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TodoView = () => {
+    
+    return (
+        <div>
+            <h2>Todo List by React Redux Toolkit</h2>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default TodoView;
