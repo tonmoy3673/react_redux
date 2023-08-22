bcd1656369c9321c926f91e2f13f01e4d9d842b0
tonmoy <tonmoy3673@gmail.com>
2023-08-22 23:17:08 +0600

    remove slice

--- src/Component/Comments/CommentSlice.js
+++ src/Component/Comments/CommentSlice.js
@@ -1,19 +1,2 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
 
-export const fetchComments=createAsyncThunk('comments/fetchComments',async()=>{
-const res=await axios.get('https://jsonplaceholder.typicode.com/comments')
-});
-
-export const CommentSlice =createSlice({
-    name:'Comments',
-    initialState:{
-        isLoading:false,
-        comments:[],
-        error:null
-    },
-    extraReducers:(builder)=>{
-    
-    }
-});
 
