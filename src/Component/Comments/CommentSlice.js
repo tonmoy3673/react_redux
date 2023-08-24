8a1aa75d4ade32f20c27f76a5ad34ae39edc8981
tonmoy <tonmoy3673@gmail.com>
2023-08-24 23:18:14 +0600

    set state

--- src/Component/Comments/CommentSlice.js
+++ src/Component/Comments/CommentSlice.js
@@ -1,7 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-
 export const CommentSlice=createSlice({
-    
+    name:'comments',
+    initialState:{
+        isLoading:false,
+        comments:[],
+        error:null
+    }
 })
