440d8d6bd91b336f58cbaaba6b5f63311f3d9fed
tonmoy <tonmoy3673@gmail.com>
2023-09-02 23:22:07 +0600

    create counter file

---
+++ src/Component/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <h2>React Redux Counter </h2>
+            
+        </div>
+    );
+};
+
+export default Counter;
