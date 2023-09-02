fe1f1a4709b4290ec752f939c4cfd393899de15f
tonmoy <tonmoy3673@gmail.com>
2023-09-03 03:08:42 +0600

    implemented add,reset,remove button

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD, REMOVE, RESET } from './CounterSlice';
 
 const Counter = () => {
+    const {count}=useSelector((state)=>(state.count));
+   
+    const dispatch=useDispatch();
+    
+
     return (
         <div>
             <h2>React Redux Counter </h2>
-            <h3>Count = 0</h3>
+            <h3>Count = {count}</h3>
+            <section>
+                <button onClick={()=>dispatch(ADD())}>Add</button>
+                <button onClick={()=>dispatch(RESET())}>Reset</button>
+                <button onClick={()=>dispatch(REMOVE())}>Remove</button>
+            </section>
         </div>
     );
 };
