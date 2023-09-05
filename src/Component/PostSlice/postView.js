09a2541daf68b976f5aeb3037adf0a2132efa6d7
tonmoy <tonmoy3673@gmail.com>
2023-09-05 23:25:13 +0600

    set loading & error

--- src/Component/PostSlice/postView.js
+++ src/Component/PostSlice/postView.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const PostView = () => {
-    const {isLoading,error,post}=useSelector((state)=>(state.post))
+    const {isLoading,error,post}=useSelector((state)=>(state.post));
+
+
     return (
         <div>
             <h2>All Posts</h2>
+            <section>
+                {
+                    isLoading && <h4>... Loading </h4>
+                }
+
+                {
+                    error && <h4>{error.message}</h4>
+                }
+            </section>
         </div>
     );
 };
