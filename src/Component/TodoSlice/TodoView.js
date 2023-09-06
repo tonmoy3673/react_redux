004cf1b4375855a9565eb5d798b2bd48676dbf76
tonmoy <tonmoy3673@gmail.com>
2023-09-07 00:13:19 +0600

    set error and loading

--- src/Component/TodoSlice/TodoView.js
+++ src/Component/TodoSlice/TodoView.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const TodoView = () => {
+    const {isLoading,error,todo}=useSelector((state)=>(state.todo));
+
     return (
         <div>
             <h2>Todo Lists</h2>
+            <section>
+                {
+                    isLoading && <h4>Loading ........ </h4>
+                }
+                {
+                    error && <h4>{error.message}</h4>
+                }
+            </section>
+
         </div>
     );
 };
