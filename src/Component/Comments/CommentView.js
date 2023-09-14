3fd7403fce575ec90efcd46ba9a143d0850834e0
tonmoy <tonmoy3673@gmail.com>
2023-09-14 23:15:37 +0600

    use dispatch

--- src/Component/Comments/CommentView.js
+++ src/Component/Comments/CommentView.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { fetchComment } from './CommentSlice';
 
 const CommentView = () => {
     const {isLoading,comment,error}=useSelector((state)=>(state.comment));
     const dispatch=useDispatch();
-    
+
+    useEffect(()=>{
+        dispatch(fetchComment())
+    },[]);
 
     return (
         <div>
             <h3>All Comments List</h3>
             <section>
+            {
 
+            }
             </section>
         </div>
     );
