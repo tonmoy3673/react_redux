773497914f34fee358efce0ec26e704b1449fc37
tonmoy <tonmoy3673@gmail.com>
2023-09-17 01:07:54 +0600

    set counterReducer

---
+++ src/Component/CounterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const CounterSlice=createSlice({
+    name:'count',
+    initialState:{
+        count:0
+    },
+    reducers:{
+    add:(state)=>{
+        state.count=state.count+1
+    },
+    reset:(state)=>{
+        state.count=0
+    },
+    remove:(state)=>{
+        state.count=state.count-1
+    }
+
+    
+    }
+});
+
+export const {add,reset,remove}=CounterSlice.actions;
+export default CounterSlice.reducer;
