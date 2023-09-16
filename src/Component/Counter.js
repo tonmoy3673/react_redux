6e3b722bb030a405a4fb7f66e461ba8e4cb8c180
tonmoy <tonmoy3673@gmail.com>
2023-09-17 01:09:32 +0600

    set store

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Counter = () => {
+    
     return (
         <div>
             <section>
--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import PostReducer from "../PostSlice/PostSlice";
 import TodoReducer from "../TodoSlice/TodoSlice";
 import CommentReducer from "../Comments/CommentSlice";
+import CounterReducer from "../CounterSlice";
 
 
 export const store=configureStore({
     reducer:{
+        count:CounterReducer,
         post:PostReducer,
         todo:TodoReducer,
         comment:CommentReducer
