60991aed8347c09d0628576472614cc05988a4d2
tonmoy <tonmoy3673@gmail.com>
2023-09-16 23:47:40 +0600

    set title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import CommentView from './Component/Comments/CommentView';
+import Counter from './Component/Counter';
 import PostView from './Component/PostSlice/PostView';
 import TodoView from './Component/TodoSlice/TodoView';
 
@@ -12,9 +13,11 @@ function App() {
   return (
     <div className="App">
       <h1>React-Redux</h1>
+      <Counter/>
      <TodoView/>
      <PostView/>
     <CommentView/>
+
  
     </div>
   );
---
+++ src/Component/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <section>
+                <h2>React Redux Counter App</h2>
+            </section>
+        </div>
+    );
+};
+
+export default Counter;
