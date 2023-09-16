af96dc39aaa4a09257d3df1cb990a79628b3d679
tonmoy <tonmoy3673@gmail.com>
2023-09-17 01:13:39 +0600

    implemented add

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { add } from './CounterSlice';
 
 const Counter = () => {
-    
+    const {count}=useSelector((state)=>(state.count));
+    const dispatch=useDispatch();
     return (
         <div>
             <section>
                 <h2>React Redux Counter App</h2>
                 <div>
-                    <h3>Count : 0 </h3>
+                    <h3>Count : {count} </h3>
+                    <button onClick={()=>dispatch(add())}>Add</button>
                 </div>
             </section>
         </div>
