334e59f8657560bb4eb8991d2f5b6da4a575ac92
tonmoy <tonmoy3673@gmail.com>
2023-09-16 23:48:55 +0600

    set count

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -5,6 +5,9 @@ const Counter = () => {
         <div>
             <section>
                 <h2>React Redux Counter App</h2>
+                <div>
+                    <h3>Count : 0 </h3>
+                </div>
             </section>
         </div>
     );
