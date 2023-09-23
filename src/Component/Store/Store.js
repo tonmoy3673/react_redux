3aafee33c950ce9adcfafa869a9aa18c237e5532
tonmoy <tonmoy3673@gmail.com>
2023-09-23 23:36:55 +0600

    modify store

--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import PostReducer from "../PostSlice/PostSlice";
-import TodoReducer from "../TodoSlice/TodoSlice";
 import CommentReducer from "../Comments/CommentSlice";
 import CounterReducer from "../CounterSlice";
 
