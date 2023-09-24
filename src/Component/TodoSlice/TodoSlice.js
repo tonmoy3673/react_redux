facadf81158cee17a97b9f23cd5e2b99f41288bc
tonmoy <tonmoy3673@gmail.com>
2023-09-24 23:58:36 +0600

    set API

--- src/Component/TodoSlice/TodoSlice.js
+++ src/Component/TodoSlice/TodoSlice.js
@@ -1,7 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const fetchTodo=createAsyncThunk(('todo/fetchTodo',async()=>{
-    
+    const res=await axios.get('https://jsonplaceholder.typicode.com/todos');
+    return res.data
 }))
 
 export const TodoSlice=createSlice({
