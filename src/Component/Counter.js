d2a762221b5b82bbd00e0a738306d7449ca384eb
tonmoy <tonmoy3673@gmail.com>
2023-10-25 00:38:26 +0600

    set counterSlice

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { add, add10, remove, reset } from './CounterSlice';
+
 
 const Counter = () => {
-    const {count}=useSelector((state)=>(state.count));
-    const dispatch=useDispatch();
+    
     return (
         <div>
             <section>
                 <h2>React Redux Counter App</h2>
                 <div>
-                    <h3>Count : {count} </h3>
-                    <button onClick={()=>dispatch(add())}>Add</button>
-                    <button onClick={()=>dispatch(reset())}>Reset</button>
-                    <button onClick={()=>dispatch(remove())}>Reset</button>
-                    <button onClick={()=>dispatch(add10(10))}>Add10</button>
+                    <h3>Count : 0 </h3>
+                    
                 </div>
             </section>
         </div>
--- src/Component/CounterSlice.js
+++ src/Component/CounterSlice.js
@@ -7,22 +7,18 @@ export const CounterSlice=createSlice({
         count:0
     },
     reducers:{
-    add:(state)=>{
-        state.count=state.count+1
-    },
-    reset:(state)=>{
-        state.count=0
-    },
-    remove:(state)=>{
-        state.count=state.count-1
-    },
-    add10:(state,action)=>{
-        state.count=state.count+action.payload
-    }
-
-    
+        add:(state)=>{
+            state.count=state.count+1
+        },
+        reset:(state)=>{
+            state.count=0
+        },
+        remove:(state)=>{
+            state.count=state.count-1
+        }
+        
     }
 });
 
-export const {add,reset,remove,add10}=CounterSlice.actions;
+export const {add,remove,reset}=CounterSlice.actions;
 export default CounterSlice.reducer;
