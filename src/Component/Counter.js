bddb828e67cebfe851c8faa2e5fb656eba9116d6
tonmoy <tonmoy3673@gmail.com>
2023-10-25 00:51:48 +0600

    installed tailwind

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -8,9 +8,12 @@ console.log(count)
     return (
         <div>
             <section>
-                <h2>React Redux Counter App</h2>
+                <h2 className='text-2xl font-semibold'>React Redux Counter App</h2>
                 <div>
-                    <h3>Count : 0 </h3>
+                    <h3>Count : {count} </h3>
+                    <div>
+                        <button className=''>AddCount</button>
+                    </div>
                     
                 </div>
             </section>
