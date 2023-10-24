2bcf053ba47d9b0ec6de42c10a23db063d0bf4fa
tonmoy <tonmoy3673@gmail.com>
2023-10-25 01:51:21 +0600

    implemeted reset & remove count

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import CounterSlice from './CounterSlice';
-import {add} from './CounterSlice'
+
+import {add,reset,remove} from './CounterSlice';
 
 
 const Counter = () => {
-const {count}=useSelector((state)=>(state.count));
-const dispatch=useDispatch();
-
-console.log(count)
+    const {count}=useSelector((state)=>(state.count));
+    console.log(count);
+    const dispatch=useDispatch()
     return (
         <div className='py-2'>
-            <section>
-                <h2 className='text-xl py-4 font-semibold'>React Redux Counter App</h2>
-                <div>
-                    <h3 className='text-lg'>Count : <span className='font-semibold'>{count} </span></h3>
-                    <div className='py-5'>
-                    <button type="button" class="text-white bg-purple-700 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 me-4" onClick={()=>dispatch(add())}>addCount</button>
-                    <button type="button" class="text-white bg-purple-700 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 me-4">resetCount</button>
-                    <button type="button" class="text-white bg-purple-700 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 ">removeCount</button>
+        <section>
+            <h2 className='text-xl py-4 font-semibold'>React Redux Counter App</h2>
+            <div>
+                <h3 className='text-lg'>Count : <span className='font-semibold'>{count} </span></h3>
+                <div className='py-5'>
+                <button type="button" className="text-white bg-purple-700 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 me-4" onClick={()=>dispatch(add())}>addCount</button>
+                <button type="button" className="text-white bg-purple-700 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 me-4" onClick={()=>dispatch(reset())}>resetCount</button>
+                <button type="button" className="text-white bg-purple-700 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 " onClick={()=>dispatch(remove())}>removeCount</button>
 
-                    </div>
-                    
                 </div>
-            </section>
-        </div>
+                
+            </div>
+        </section>
+    </div>
     );
 };
 
