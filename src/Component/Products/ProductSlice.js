fa4efc56a5eb8d64e3ac07206cae3cc84e091e22
tonmoy <tonmoy3673@gmail.com>
2023-10-30 22:13:32 +0600

    set product reducer

--- src/Component/Products/ProductSlice.js
+++ src/Component/Products/ProductSlice.js
@@ -1,19 +1,39 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 
 export const fetchProduct=createAsyncThunk('products/fetchProduct',async()=>{
-    
+    const res= await axios.get('/products.json');
+    const data= await res.json();
+    console.log(data);
 })
 
 
 export const ProductSlice = createSlice({
     name:'products',
     initialState:{
-        isLoading:true,
+        isLoading:false,
         products:[],
         error:null,
     },
     extraReducers:builder=>{
+        builder.addCase(fetchProduct.pending,(state)=>{
+            state.isLoading=true;
+            state.products=[];
+            state.error=null;
 
+        });
+        builder.addCase(fetchProduct.fulfilled,(state,action)=>{
+            state.isLoading=false;
+            state.products=action.payload;
+            state.error=null;
+        });
+        builder.addCase(fetchProduct.rejected,(state,action)=>{
+            state.isLoading=false;
+            state.products=[];
+            state.error=action.error.message;
+        })
     }
-})
+});
+
+export default ProductSlice.reducer;
